e96c63568a113f6b89c179af0d56e627480940f9
AbduPY123 <abdurahim_209@mail.ru>
2022-08-26 12:51:39 +0300

    lesson3

---
+++ lesson_3/lesson3/libraty/authors/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer,HyperlinkedModelSerializer
+
+from .models import Author,Book,Biography
+
+class AuthorModelSerializer(ModelSerializer):
+
+    class Meta:
+        model = Author
+        fields = '__all__'
+        # fields = ('first_name',)
+        # exclude = ('first_name',)
+
+class BiographyModelSerializer(HyperlinkedModelSerializer):
+    # author = AuthorModelSerializer()
+    class Meta:
+        model = Biography
+        fields = '__all__'
+
+class BookModelSerializer(ModelSerializer):
+
+    authors = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Book
+        fields = '__all__'
